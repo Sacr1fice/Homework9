62f8df7aa1689e29b311e068af12a06cff70eda7
Sacr1fice <sanunkurov@gmail.com>
2023-07-11 13:49:04 +0300

    first commit

---
+++ src/main/java/ru/netology/Main.java
@@ -0,0 +1,19 @@
+package ru.netology;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Post post = new Post();
+                post.name = "Иван";
+        post.surname = "Иванов";
+        post.patronymic = "Иванович";
+        post.passport = "1234 № 987654";
+        post.phone = "+7-(990)-546-77-99";
+        post.subscription = "true";
+        post.birthday = new FormDate();
+        post.birthday.day = 13;
+        post.birthday.month = 6;
+        post.birthday.year = 1996;
+
+    }
+}
